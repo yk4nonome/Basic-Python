9fe0e36108379a3ae69c30a81522d0a958feb566
yk4nonome <y.kawamura@4nonome.com>
2022-07-03 13:34:42 +0900

    first commit

--- AOJ/02/02_A.py
+++ AOJ/02/02_A.py
@@ -1,9 +1,9 @@
-a,b=input().split()
-a=int(a)
-b=int(b)
-if a>b:
- print('a > b')
-elif a==b:
+a, b = input().split()
+a = int(a)
+b = int(b)
+if a > b:
+    print('a > b')
+elif a == b:
     print('a == b')
 else:
     print('a < b')
--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,16 +1,16 @@
-a,b,c==input().split()
-a=int(a)
-b=int(b)
-c=int(c)
-if a>=b>=c:
- print(c,b,a)
-elif a>=c>=b:
-    print(b,c,a)
-elif b>=a>=c:
-        print(c,a,b) 
-elif b>=c>=a:
-      print(a,c,b)
-elif c>=b>=a:
-    print(a,b,c)
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+if a >= b >= c:
+    print(c, b, a)
+elif a >= c >= b:
+    print(b, c, a)
+elif b >= a >= c:
+    print(c, a, b) 
+elif b >= c >= a:
+    print(a, c, b)
+elif c >= b >= a:
+    print(a, b, c)
 else:
- print(b,a,c)
+    print(b, a, c)
